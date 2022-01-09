193a02fdf9059071dafefceb487cccb7116827f5
AutismPrime <lazarovmihail97@gmail.com>
2022-01-09 18:51:35 +0200

    commit3

---
+++ Tasks/beatles_band.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+beatles = []
+print("Step 1:", beatles)
+
+beatles.append("John Lennon")
+beatles.append("Paul McCartney")
+beatles.append("George Harrison")
+print("Step 2:", beatles)
+
+for i in range(2):
+    a = input("Add the next beatle: ")
+    beatles.append(a)
+print("Step 3:", beatles)
+
+del beatles[4]
+del beatles[3]
+print("Step 4:", beatles)
+
+beatles.insert(0, "Ringo Starr")
+print("Step 5:", beatles)
+
+
+# testing list legth
+print("The Fab", len(beatles))
