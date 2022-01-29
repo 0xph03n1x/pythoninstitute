5a73e28b97ec1da4b63a60359db176cea15f08f0
AutismPrime <lazarovmihail97@gmail.com>
2022-01-29 13:08:25 +0200

    m

---
+++ Python Essentials 1 (Basics, v.2.0)/Module4-Functions/multiple_parameters.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def message(what, number):
+    print("Enter", what, "number:", number)
+
+# invoke the function
+message("telephone", 11)
+message("price", 5)
+message("the word", "number")
