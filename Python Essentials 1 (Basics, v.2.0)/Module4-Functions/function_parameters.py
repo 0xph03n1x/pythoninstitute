4954092ccd6bef8cb8b18fff184763fc3159356d
AutismPrime <lazarovmihail97@gmail.com>
2022-01-29 13:03:58 +0200

    m

---
+++ Python Essentials 1 (Basics, v.2.0)/Module4-Functions/function_parameters.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def message(number):
+    print("Enter a number:", number)
+
+message(1)
---
+++ Python Essentials 1 (Basics, v.2.0)/Module4-Functions/function_shadowing.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def message(number):
+    print("Enter a number:", number)
+
+number = 1234
+message(1)
+print(number)
+
+#Since the parameter is only valid in the defined function, having a variable with the same name is completely legal.
